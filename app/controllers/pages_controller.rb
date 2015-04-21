2ce3da449af019b7aefdf094af15818ffccbde6e
Asif Hazrat <asifhazrat@gmail.com>
2015-04-21 05:33:18 +0000

    update events page and styles

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,4 +13,24 @@ class PagesController < ApplicationController
 
   def stories
   end
+  
+  @pictures = [
+    {
+      title: 'image one',
+      url: 'http://i.imgur.com/QCSlS3o.jpg',
+      thumb: 'http://i.imgur.com/QCSlS3o.jpg',
+      scale: 100,
+      hpan: 50,
+      vpan: 50
+    },
+    {
+      title: 'image one',
+      url: 'http://i.imgur.com/QCSlS3o.jpg',
+      thumb: 'http://i.imgur.com/QCSlS3o.jpg',
+      scale: 100,
+      hpan: 50,
+      vpan: 50
+    }
+  ]
+  
 end
